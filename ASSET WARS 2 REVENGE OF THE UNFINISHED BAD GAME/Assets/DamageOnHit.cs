4bff628c8dc01529f2005b94a94222905819545b
PiotrusWatson <piotruswatson@gmail.com>
2017-09-10 14:45:10 +0100

    made spawn happen

---
+++ ASSET WARS 2 REVENGE OF THE UNFINISHED BAD GAME/Assets/DamageOnHit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnHit : MonoBehaviour {
+	public float damage = 30;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ ASSET WARS 2 REVENGE OF THE UNFINISHED BAD GAME/Assets/Scripts/Health.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	[SerializeField]
+	float maxHealth = 100;
+	float health;
+	bool isDead;
+
+	// Use this for initialization
+	void Start () {
+		health = maxHealth;
+		isDead = false;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D c) {
+		if (c.GetComponent<DamageOnHit>()) {
+			if (!isDead)
+				health -= (c.GetComponent<DamageOnHit> ().damage);
+		}
+	}
+
+	void Update(){
+		if (health <= 0) {
+			Die ();
+		}
+	}
+
+	public void Die(){
+		isDead = true;
+	}
+}
