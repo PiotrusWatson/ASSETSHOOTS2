39c2c80206489497e632239bb99023ef78b6268d
Tom <tom.f.whitehouse@btinternet.com>
2017-09-11 22:00:57 +0100

    Explosions

---
+++ ASSET WARS 2 REVENGE OF THE UNFINISHED BAD GAME/Assets/Scripts/ExplosionBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionBehaviour : MonoBehaviour {
+
+	private AnimationClip anim;
+	private AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>().runtimeAnimatorController.animationClips[0];
+		audio = GetComponent<AudioSource>();
+		StartCoroutine (doAnimation());
+		StartCoroutine (doDestroy ());
+	}
+
+	private IEnumerator doAnimation(){
+		yield return new WaitForSeconds(anim.length);
+		Destroy (GetComponent<SpriteRenderer>());
+	}
+
+	private IEnumerator doDestroy(){
+		yield return new WaitForSeconds (Mathf.Max (anim.length, audio.clip.length));
+		Destroy (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
--- ASSET WARS 2 REVENGE OF THE UNFINISHED BAD GAME/Assets/Scripts/GenericEnemy/EnemyHealth.cs
+++ ASSET WARS 2 REVENGE OF THE UNFINISHED BAD GAME/Assets/Scripts/GenericEnemy/EnemyHealth.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHealth : Health {
+	public GameObject explosion;
+
 	void Awake(){
 		GetComponent<CommonVars> ().team = Team.Enemy;
 	}
@@ -12,6 +14,7 @@ public class EnemyHealth : Health {
 		base.Update();
 
 		if (isDead==true) {
+			Instantiate(explosion, gameObject.transform.position, Quaternion.Euler (0,0,90));
 			Destroy (gameObject);
 		}
 	}
