1622ce3752f33beef5e055b55de40b96a5ec6215
Tom <tom.f.whitehouse@btinternet.com>
2017-09-11 22:56:57 +0100

    Fixed enemy overlap

---
+++ ASSET WARS 2 REVENGE OF THE UNFINISHED BAD GAME/Assets/Scripts/GenericEnemy/SpawnResolver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnResolver : MonoBehaviour {
+	public float startTime;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D c){
+		if (c.gameObject.tag == "Enemy") {
+			if (Random.Range(1,10) <= 5) {
+				Vector3 pos = transform.position;
+				pos.x += 50;
+				transform.position = pos;
+			}
+		}
+	}
+}
